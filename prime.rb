bbb084d404e173c316d9007a39f38760070e2c8c
Arslan Ali <ali.arslan319@gmail.com>
2019-04-12 00:15:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,19 @@
-def prime?(num)
-    if num < 2
-      return false
-    (2..num - 1).each do |x|
+def prime?(number)
+    return false if number < 2
+    (2..number - 1).each do |x|
         if (num % x) == 0
             return false
         end
     end
     true
 end
+
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).to_a.all? do |possible_factor|
+#       num % possible_factor != 0
+#     end
+#   end
+# end
